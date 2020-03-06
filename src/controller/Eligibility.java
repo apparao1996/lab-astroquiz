e07a9b90ed018220a2c633bb82106b1cbbf5cfd4
MALLAVALLI APPARAQ <54364675+14761A04E9@users.noreply.github.com>
2020-03-06 15:12:11 +0530

    completed

--- src/controller/Eligibility.java
+++ src/controller/Eligibility.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import utility.EligibilityCheck;
-import utility.EligibilityCheck;
+
 
 @WebServlet(urlPatterns= {"/eligiblemain"})
 public class Eligibility extends HttpServlet {
--- src/controller/EligibilityViewController.java
+++ src/controller/EligibilityViewController.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import model.User;
-import utility.BasicEligibility;
+
 import utility.EligibilityCheck;
 
 
